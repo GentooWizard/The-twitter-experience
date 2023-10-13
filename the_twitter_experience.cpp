84b8f72c1489d17efa76a9db4d20ff7b21107b3b
GentooWizard <demidfr@proton.me>
2023-10-13 20:47:22 +0300

    On branch main  Your branch is up to date with 'origin/main'.  Changes to be committed: 	modified:   the_twitter_experience.cpp

--- the_twitter_experience.cpp
+++ the_twitter_experience.cpp
@@ -2,23 +2,21 @@
 #include <iostream>
 using namespace std;
 int main() {
-	char a;
-	cout << "A twitter user is aproching you / Do you talk to him Y/N" << endl;
-	cin >> a;
-	if (a == 'N') {
+	char input;
+	cout << "A twitter user is aproching you / Do you talk to him (Y/N): ";
+	cin >> input;
+	if (input == 'N' || input == 'n') {
 		return 0;
 	}
-	if (a == 'Y') {
-		char b;
-		cout << "The twitter user asks you if you support the LGBTQIA+ community / Do you? Y/N" << endl;
-		cin >> b;
-		if (b == 'N') {
+	if (input == 'Y' || input == 'y') {
+		cout << "The twitter user asks you if you support the LGBTQIA+ community / Do you? (Y/N): ";
+		cin >> input;
+		if (input == 'N' || input == 'n') {
 			cout << "You got cancelled lol";
 			return 0;
 		}
-		if (b == 'Y') {
-			cout << "YOU WON TWITTER YAY";
-			return 0;
+		if (input == 'Y' || input == 'y') {
+			cout << "YOU WON TWITTER YAY" << endl;
 		}
 	}
 	return 0;
